982fed09e8e4207a0df2031ac88b671279a69bf2
StavKinrot <stav.kin1999@gmail.com>
2025-01-17 22:44:43 +0200

    Remove Firebase Firestore initialization code

--- insight-backend/firebase/firestore.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-key.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
-module.exports = db;
